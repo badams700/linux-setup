cac217837c6f1233c4be8bb45dc576647e634823
badams700 <badams700@gmail.com>
2026-07-10 16:11:59 -0700

    Update calibre installer command in script
    Removed sudo from calibre installer command.

--- test script.sh
+++ test script.sh
@@ -21,7 +21,7 @@ echo 'SigLevel = Required TrustedOnly' | tee -a /etc/pacman.conf
 echo 'Server = https://repo.cider.sh/arch' | tee -a /etc/pacman.conf
 pacman -Syu --noconfirm flatpak obs-studio-browser amdgpu_top blender calf cava cdrdao cdrtools cider cmake deja-dup discord discover dolphin-plugins dvd+rw-tools dysk easyeffects extra-cmake-modules ffmpeg gimp go handbrake jre-openjdk k3b lsp-plugins-lv2 mda.lv2 ntfs-3g ntfsprogs obsidian okular onlyoffice-bin prismlauncher protonplus proton-pass proton-vpn-gtk-app rpi-imager terminus-font thunderbird transmission-gtk ttf-noto-nerd vlc zam-plugins
 yay -S --noconfirm darkly google-chrome kwin-effects-better-blur-dx qdiskinfo twintaillauncher-bin visual-studio-code-bin xivlauncher zoom
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin
 flatpak install -y flathub io.github.maniacx.BudsLink io.github.wartybix.Constrict com.github.huluti.Curtail com.github.tchx84.Flatseal com.github.tenderowl.frog it.mijorus.gearlever org.jellyfin.JellyfinDesktop com.makemkv.MakeMKV io.github.alainm23.planify com.yubico.yubioauth app.zen_browser.zen
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/JackHack96/EasyEffects-Presets/master/install.sh)"
 echo 'FONT=ter-132b' | tee -a /etc/vconsole.conf
