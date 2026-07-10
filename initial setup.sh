1096f768fb30ed9a138db88211ad3d1bbdd0ec25
badams700 <badams700@gmail.com>
2026-07-10 14:40:15 -0700

    Modify limine.conf settings in setup script
    Update limine.conf settings for timeout and background.

--- initial setup.sh
+++ initial setup.sh
@@ -7,7 +7,9 @@ sudo wget https://raw.githubusercontent.com/badams700/linux-setup/main/Files/lim
 sudo pacman -R linux-cachyos-lts linux-cachyos-lts-headers \
 sudo pacman -S linux-cachyos-rc linux-cachyos-rc-headers \
 sudo pacman -S cachyos-gaming-meta cachyos-gaming-applications 
-# add term_background: 00000000 and comment wallpaper line in /boot/limine.conf
+sudo sed -i '1c\timeout: 1' /boot/limine.conf
+sudo sed -i '[]c\term_background: 00000000' /boot/limine.conf
+sudo sed -i '[]c\[wallpaper syntax]' /boot/limine.conf
 sudo pacman -S sbctl \
 sudo sbctl create-keys \
 sudo sbctl enroll-keys -m -f \
