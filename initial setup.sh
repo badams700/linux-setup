8a2d7d4b5634ee32e80f62ffd310f0bb84b3a2c5
badams700 <badams700@gmail.com>
2026-07-11 11:10:39 -0700

    Modify initial setup script for system configuration
    Updated the setup script to include new package installations and configuration changes for systemd and mkinitcpio.

--- initial setup.sh
+++ initial setup.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 timedatectl set-local-rtc 1
-cp /etc/limine-snapper-sync.conf /etc/limine-snapper-sync.conf.orig
-cd /etc
-rm limine-snapper-sync.conf
-wget https://raw.githubusercontent.com/badams700/linux-setup/main/Files/limine-snapper-sync.conf
-pacman -R --noconfirm linux-cachyos-lts linux-cachyos-lts-headers
-pacman -S --noconfirm linux-cachyos-rc linux-cachyos-rc-headers cachyos-gaming-meta cachyos-gaming-applications sbctl
-sed -i '1c\timeout: 1' /boot/limine.conf
-sed -i '[]c\term_background: 00000000' /boot/limine.conf
-sed -i '[]c\[wallpaper syntax]' /boot/limine.conf
+pacman -S --noconfirm sbctl systemd-ukify
+sed -i '2c\timeout 1' /boot/loader/loader.conf
+sed -i '3c\console-mode max' /boot/loader/loader.conf
+sed -i '11c\#default_image="/boot/initramfs-linux-cachyos.img"' /etc/mkinitcpio.d/linux-cachyos.preset
+sed -i '12c\default_uki="/boot/EFI/Linux/cachyos.efi"' /etc/mkinitcpio.d/linux-cachyos.preset
+sed -i '11c\#default_image="/boot/initramfs-linux-cachyos-rc.img"' /etc/mkinitcpio.d/linux-cachyos-rc.preset
+sed -i '12c\default_uki="/boot/EFI/Linux/cachyos-rc.efi"' /etc/mkinitcpio.d/linux-cachyos-rc.preset
+sed -i 's/$/ acpi_enforce_resources=lax amdgpu.dcfeaturemask=0x402/' /etc/kernel/cmdline
 sbctl create-keys
 sbctl enroll-keys -m -f
-sed -i '/^CMDLINE=/s/"[^"]*"/"& acpi_enforce_resources=lax amdgpu.dcfeaturemask=0x402"/' /etc/default/limine
-echo 'ENABLE_ENROLL_LIMINE_CONFIG=yes' | tee -a /etc/default/limine
-limine-enroll-config
-limine-update
+sbctl-batch-sign
 systemctl reboot --firmware-setup
